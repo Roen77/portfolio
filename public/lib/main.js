a66ed6db6d46d7b5c32256f4b4a38983d039c9ee
Taegyeong Han <65719512+Ronis-han@users.noreply.github.com>
2021-10-17 00:01:53 +0900

    first commit

--- public/lib/main.js
+++ public/lib/main.js
@@ -1,86 +1,247 @@
-'use strict';
+"use strict";
 
 (function () {
-    gsap.core.globals("ScrollTrigger", ScrollTrigger);
-    var imgElem = document.querySelector('.intro');
-    var body = document.querySelector('body');
-    var mainSection = document.querySelector('.main_section');
-    var totalCount = 97;
-    var videoFrame = [];
-    var loadCount = 0;
-    var currentFrame = void 0;
-    var setImg = function setImg() {
-        for (var i = 0; i < totalCount; i++) {
-            var img = new Image();
-            img.src = 'public/video/img_' + i + '.jpg';
-            videoFrame.push(img);
+  gsap.core.globals("ScrollTrigger", ScrollTrigger); // const imgElem=document.querySelector('.intro');
+  // const body=document.querySelector('body');
 
-            window.addEventListener("load", function () {
-                loadCount++;
-                // 이미지 로드 완료
-                if (loadCount === totalCount) {
-                    body.classList.add('after-load');
-                }
-            });
-        }
-    };
-    var init = function init() {
-        var mainSectionHeight = mainSection.scrollTop + mainSection.offsetHeight;
-        var per = pageYOffset / mainSectionHeight;
-        if (per > 1) per = 1;
-        if (per < 0) per = 0;
+  var cards = document.querySelectorAll(".main_cards .card");
+  var myProject = document.querySelector(".myproject");
+  var bar = document.querySelector(".loading_bar");
+  var intro = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.main_section',
+      pin: ".main_section",
+      scrub: 1 // markers:{
+      //     startColor:'blue',
+      //     endColor:'red'
+      // },
 
-        requestAnimationFrame(function () {
-            currentFrame = Math.round((totalCount - 1) * per);
-            imgElem.src = videoFrame[currentFrame].src;
-        });
-    };
-    var intro = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.main_section',
-            pin: ".main_section",
-            scrub: 1,
-            // markers:{
-            //     startColor:'blue',
-            //     endColor:'red'
-            // },
-            onUpdate: function onUpdate() {
-                if (body.classList.contains('after-load')) {
-                    init();
-                }
-            }
-        }
-    });
-    intro.to('.main_txt', 0.5, {
-        opacity: 0
-    });
-    var introduce = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.fix.introduce',
-            pin: '.fix.introduce',
-            start: 'top top',
-            end: '+=18000',
-            scrub: 1
-        }
-    });
-    introduce.to('body', 0.3, { backgroundColor: '#BBDEFB' }).to('.introduce .bg_main', 0.5, { opacity: 1, scale: 2, ease: "circ.out" }).to('.introduce .txt-1', 0.3, { opacity: 1 }).to('.introduce .txt-1', 0.3, { top: '3%' }, '-=0.2').to('.main .person', 0.5, { opacity: 1, scale: 0.9 }, '-=0.2').to('.introduce .txt-1,.main .person', 0.3, { opacity: 0 }).to('.introduce .bg_main', 0.3, { opacity: 0 }, '-=0.1').to('.introduce .bg_work', 0.3, { opacity: 1 }, '-=0.2').to('.introduce .txt-2', 0.3, { opacity: 1 }).to('.introduce .txt-2', 0.3, { top: '2%' }, '-=0.2').to('body', 0.3, { backgroundColor: '#EDE7F6' }).to('.work .person', 0.5, { opacity: 1, scale: 1.1 }, '-=0.2').to('.work .person', 0.4, { opacity: 0 }).to('.introduce .txt-3', 0.3, { opacity: 1 }).to('.introduce .txt-3', 0.3, { top: '15%' }, '-=0.2').to('.qna .person', 0.4, { opacity: 1 }, '-=0.1').to('.introduce .word01', 0.3, { opacity: 1 }).to('.introduce .word01', 0.3, { y: -10 }, '-=0.2').to('.introduce .word02', 0.3, { opacity: 1 }).to('.introduce .word02', 0.3, { y: -10 }, '-=0.2').to('.introduce .word03', 0.3, { opacity: 1 }).to('.introduce .word03', 0.3, { y: -10 }, '-=0.2').to('.introduce .txt-2,.introduce .txt-3,.word01,.word02,.word03', 0.3, { opacity: 0, y: 100 }).to('.introduce .bg_work,.qna .person', 0.3, { opacity: 0 })
-    // .to('.introduce .house',0.3,{opacity:1,scale:1.5})
-    .addLabel("start").to('.introduce .txt-4', 0.3, { opacity: 1, y: 20 }).to('.introduce .main-circle', 0.3, { opacity: 1 }).to('.introduce .circle-container,.introduce .line1', 0.7, { rotate: '360deg', repeat: 4 }).to('.introduce .line2', 0.7, { rotate: '10deg' }, '-=0.6').to('.introduce .circle1,.summer', 0.2, { opacity: 1 }, '-=1.5').to('.introduce .circle2,.fall', 0.2, { opacity: 1 }, '-=1').to('.winter', 0.2, { opacity: 1 }, '-=0.2').addLabel("end").to('.introduce .line1,.center-circle,.introduce .line2', 0.6, { opacity: 1 }, '-=3.5').to('.work .person', 0.1, { opacity: 1 }, '-=3').to('.introduce .txt-5', 0.3, { opacity: 1, top: '15%' }, '-=0.2').to('.introduce .slide,.main-circle,.small-circle,.txt-4,.txt-5', 0.3, { opacity: 0 }).to('.work .person', 0.3, { opacity: 0 }).to('.introduce .txt-10', 0.3, { opacity: 1, top: '10%' }).to('.office', 0.3, { opacity: 1 }).from('.advantage .fr .number', 0.3, { opacity: 0, y: -100 }).from('.advantage .txt-11', 0.3, { opacity: 0, y: -100 }).from('.advantage .sec .number', 0.3, { opacity: 0, y: -100 }).from('.advantage .txt-12', 0.3, { opacity: 0, y: -100 }).from('.advantage .th .number', 0.3, { opacity: 0, y: -100 }).from('.advantage .txt-13', 0.3, { opacity: 0, y: -100 }).to('.advantages,.txt-10', 0.3, { opacity: 0 }).to('.txt-14', 0.3, { opacity: 1, top: '10%' }).to('.office', 0.6, { scale: 1.5, bottom: '200px' }).to('.office', 0.6, { left: '-100%' }).to('.office', 0.3, { opacity: 0 }).to('.txt-14', 0.3, { opacity: 0 }).to('.introduce .txt-6', 0.3, { opacity: 1, top: '10%' }, '-=0.2').set('body', { backgroundColor: '#F3E5F5' }).to('.ability .person', 0.3, { opacity: 1, scale: 0.8 }).to('.h-box .html', 0.3, { height: '93%' }, '-=0.2').to('.h-box span', 0.3, { y: 50, opacity: 1 }, '-=0.2').to('.c-box .css', 0.3, { height: '88%' }, '-=0.2').to('.c-box span', 0.3, { y: 50, opacity: 1 }, '-=0.2').to('.j-box .js', 0.3, { height: '84%' }, '-=0.2').to('.j-box span', 0.3, { y: 50, opacity: 1 }, '-=0.2').to('.v-box .vue', 0.3, { height: '86%' }, '-=0.2').to('.v-box span', 0.3, { y: 50, opacity: 1 }, '-=0.2').to('.level-up', 0.1, { y: -50, opacity: 1 }, '-=0.2').to('.level-up', 0.1, { opacity: 0 }).set('.skill-box>div span', { opacity: 0 }).to('.skill-box>div .bar-container', 0.3, { height: '200%' }).to('.ability .person', 0.3, { opacity: 0 }, '-=0.1').to('.super .person', 0.3, { opacity: 1, scale: 1.2 }, '-=0.2').to('.introduce .txt-6', 0.3, { opacity: 0 }).to('.introduce .txt-7', 0.3, { opacity: 1, top: '4%' }).to('.introduce .txt-8', 0.3, { opacity: 1, top: '15%' }).to('.phone-box', 0.3, { opacity: 1, y: -800, ease: Expo.easeInOut }, '-=0.1').to('.phone-box', 0.3, { scale: 1.3 }).to('.phone-box', 0.3, { y: 900, ease: Expo.easeInOut }).to('body', 0.3, { backgroundColor: '#222' });
-    // projects
-    gsap.from(".card_lib", {
-        scrollTrigger: ".card_lib",
-        x: 200,
-        opacity: 0,
-        scrub: 3,
-        duration: 1
-    });
-    gsap.from(".card_memo", {
-        scrollTrigger: ".card_memo",
-        x: 200,
-        opacity: 0,
-        scrub: 3,
-        duration: 1
-    }, 0.2);
+    }
+  });
+  intro.to('.name', 0.1, {
+    y: 160,
+    opacity: 1,
+    autoAlpha: 1,
+    ease: "bounce.out"
+  }).to('.circle_bg', 0.3, {
+    y: -20
+  }, 0.3).to('.circle_bg', 0.2, {
+    opacity: 0
+  }).to('.arrow', 0.1, {
+    opacity: 1,
+    y: 20
+  }).to('.wave', 0.3, {
+    y: -400
+  }).to('.wave_bg', 0.3, {
+    x: 70
+  }).to('.name', 0.3, {
+    y: 0,
+    opacity: 0
+  }); // introduce
+  // const introduce = gsap.timeline({
+  //     scrollTrigger: {
+  //         trigger: '.fix.introduce',
+  //         pin: '.fix.introduce',
+  //         start: 'top top',
+  //         end: '+=1000',
+  //         scrub:1
+  //     }
+  // });
+  // introduce.to('body',0.3,{backgroundColor:'#BBDEFB'})
+  // .to('.introduce .bg_main',0.5,{ opacity: 1,scale:2,ease:"circ.out"})
+  // .to('.introduce .txt-1',0.3,{opacity:1})
+  // .to('.introduce .txt-1',0.3,{top:'3%'},'-=0.2')
+  // .to('.main .person',0.5,{opacity:1,scale:0.9},'-=0.2')
+  // .to('.introduce .txt-1,.main .person',0.3,{opacity:0})
+  // .to('.introduce .bg_main',0.3,{opacity:0},'-=0.1')
+  // .to('.introduce .bg_work',0.3,{opacity:1},'-=0.2')
+  // .to('.introduce .txt-2',0.3,{opacity:1})
+  // .to('.introduce .txt-2',0.3,{top:'2%'},'-=0.2')
+  // .to('body',0.3,{backgroundColor:'#EDE7F6'})
+  // .to('.work .person',0.5,{opacity:1,scale:1.1},'-=0.2')
+  // .to('.work .person',0.4,{opacity:0})
+  // .to('.introduce .txt-3',0.3,{opacity:1})
+  // .to('.introduce .txt-3',0.3,{top:'15%'},'-=0.2')
+  // .to('.qna .person',0.4,{opacity:1},'-=0.1')
+  // .to('.introduce .word01',0.3,{opacity:1})
+  // .to('.introduce .word01',0.3,{y:-10},'-=0.2')
+  // .to('.introduce .word02',0.3,{opacity:1})
+  // .to('.introduce .word02',0.3,{y:-10},'-=0.2')
+  // .to('.introduce .word03',0.3,{opacity:1})
+  // .to('.introduce .word03',0.3,{y:-10},'-=0.2')
+  // .to('.introduce .txt-2,.introduce .txt-3,.word01,.word02,.word03',0.3,{opacity:0,y:100})
+  // .to('.introduce .bg_work,.qna .person',0.3,{opacity:0})
+  // // .to('.introduce .house',0.3,{opacity:1,scale:1.5})
+  // .addLabel("start")
+  // .to('.introduce .txt-4',0.3,{opacity:1,y:20})
+  // .to('.introduce .main-circle',0.3,{opacity:1})
+  // .to('.introduce .circle-container,.introduce .line1',0.7,{rotate:'360deg',repeat:4})
+  // .to('.introduce .line2',0.7,{rotate:'10deg'},'-=0.6')
+  // .to('.introduce .circle1,.summer',0.2,{opacity:1},'-=1.5')
+  // .to('.introduce .circle2,.fall',0.2,{opacity:1},'-=1')
+  // .to('.winter',0.2,{opacity:1},'-=0.2')
+  // .addLabel("end")
+  // .to('.introduce .line1,.center-circle,.introduce .line2',0.6,{opacity:1},'-=3.5')
+  // .to('.work .person',0.1,{opacity:1},'-=3')
+  // .to('.introduce .txt-5',0.3,{opacity:1, top:'15%'},'-=0.2')
+  // .to('.introduce .slide,.main-circle,.small-circle,.txt-4,.txt-5',0.3,{opacity:0})
+  // .to('.work .person',0.3,{opacity:0})
+  // .to('.introduce .txt-10',0.3,{opacity:1,top:'10%'})
+  // .to('.office',0.3,{opacity:1})
+  // .to('body',0.3,{backgroundColor:'#222'})
+  // advantage
 
-    setImg();
+  var advantage = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.fix.advantage',
+      pin: '.fix.advantage',
+      start: 'top top',
+      end: '+=1500',
+      scrub: 1
+    }
+  });
+  advantage.to('.advantage .main_title', 0.2, {
+    opacity: 1,
+    x: "0px"
+  }).to('.txt1', 0.1, {
+    y: 200,
+    autoAlpha: 1
+  }) // .to('.layer1',0.1,{scale:1})
+  // .to('.layer2',0.1,{scale:1})
+  .to('body', 0.3, {
+    backgroundColor: '#222'
+  }).to('.advantages', 0.3, {
+    left: "0%"
+  }).to('.text.txt-2', 0.3, {
+    opacity: 1
+  });
+  advantage.to('.ad1', 0.4, {
+    scale: 1.1
+  });
+  advantage.to('.text.txt-2', 0.3, {
+    opacity: 0
+  });
+  advantage.to('.advantages', 0.4, {
+    left: "-100%"
+  });
+  advantage.to('.text.txt-3', 0.3, {
+    opacity: 1
+  });
+  advantage.to('.ad2', 0.4, {
+    scale: 1.1
+  });
+  advantage.to('.text.txt-3', 0.3, {
+    opacity: 0
+  });
+  advantage.to('.advantages', 0.4, {
+    left: "-200%"
+  });
+  advantage.to('.ad3', 0.4, {
+    scale: 1.1
+  });
+  advantage.to('.text.txt-4', 0.3, {
+    opacity: 1
+  });
+  advantage.to('.text.txt-4', 0.3, {
+    opacity: 0
+  }); // 프로젝트
+
+  var project = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.fix.myproject',
+      pin: '.fix.myproject',
+      start: 'top top',
+      end: '+=500',
+      scrub: 1
+    }
+  });
+  project.to('.myproject .main_title', 0.2, {
+    opacity: 1,
+    x: "0px"
+  });
+  project.to('.myproject .layer_bg', 0.3, {
+    opacity: 0.2
+  });
+  project.to('.myproject .bulb1', 0.1, {
+    y: -140
+  });
+  project.to('.myproject .bulb2', 0.1, {
+    y: -100
+  });
+  project.to('.myproject .bulb3', 0.1, {
+    y: -70
+  });
+  project.to('.myproject .bulb1 .on', 0.1, {
+    opacity: 1
+  });
+  project.to('.myproject .bulb2 .on', 0.1, {
+    opacity: 1
+  });
+  project.to('.myproject .bulb3 .on', 0.1, {
+    opacity: 1
+  });
+  project.to('.card.library', 0.3, {
+    opacity: 1,
+    x: "0%",
+    y: "0%",
+    visibility: "visible",
+    rotate: "0deg"
+  });
+  project.to('.card.memo', 0.3, {
+    opacity: 1,
+    x: "0%",
+    y: "0%",
+    visibility: "visible",
+    rotate: "0deg"
+  });
+  var prevIndex;
+
+  var _loop = function _loop(i) {
+    console.log(cards[i], '카드좀');
+    cards[i].addEventListener("mouseenter", function (e) {
+      e.preventDefault();
+      console.log("click");
+      myProject.classList.add("active".concat(i));
+
+      if (prevIndex !== i) {
+        myProject.classList.remove("active".concat(prevIndex));
+      }
+
+      prevIndex = i;
+    });
+    cards[i].addEventListener("mouseleave", function (e) {
+      console.log("리브");
+      myProject.classList.remove("active".concat(prevIndex));
+    });
+  };
+
+  for (var i = 0; i < cards.length; i++) {
+    _loop(i);
+  } // cards.forEach((card,i)=>{
+  //    card.addEventListener("mouseenter",(e)=>{
+  //     e.preventDefault()
+  //     console.log("click")
+  //     myProject.classList.add(`active${i}`)
+  //     if(prevIndex !== i){
+  //         myProject.classList.remove(`active${prevIndex}`)
+  //     }
+  //     prevIndex=i
+  //    })
+  //    card.addEventListener("mouseleave",e=>{
+  //        console.log("리브")
+  //         myProject.classList.remove(`active${prevIndex}`)
+  //    })
+  // })
+
+
+  var scrollValue = function scrollValue() {
+    var scrollHeight = document.body.scrollHeight;
+    var realHeight = scrollHeight - window.innerHeight;
+    var realRatio = window.pageYOffset / realHeight * 100;
+    if (realRatio < 0) realRatio = 1;
+    if (realRatio > 100) realRatio = 100;
+    bar.style.width = "".concat(realRatio, "%");
+  };
+
+  window.addEventListener("scroll", scrollValue);
 })();
--- public/src/main.js
+++ public/src/main.js
@@ -1,38 +1,11 @@
 (()=>{
 gsap.core.globals("ScrollTrigger", ScrollTrigger);
-const imgElem=document.querySelector('.intro');
-const body=document.querySelector('body');
-const mainSection=document.querySelector('.main_section');
-const totalCount=97;
-const videoFrame=[];
-let loadCount=0;
-let currentFrame;
-const setImg = () => {
-    for (let i = 0; i < totalCount; i++) {
-        const img = new Image();
-        img.src = `public/video/img_${i}.jpg`;
-        videoFrame.push(img);
+// const imgElem=document.querySelector('.intro');
+// const body=document.querySelector('body');
+const cards=document.querySelectorAll(".main_cards .card");
+const myProject=document.querySelector(".myproject");
+const bar=document.querySelector(".loading_bar")
 
-        window.addEventListener("load", function () {
-            loadCount++;
-            // 이미지 로드 완료dd
-            if (loadCount === totalCount) {
-                body.classList.add('after-load')
-            }
-        })
-    }
-}
-const init = () => {
-    let mainSectionHeight = mainSection.scrollTop + mainSection.offsetHeight;
-    let per = pageYOffset / mainSectionHeight;
-    if (per > 1) per = 1;
-    if (per < 0) per = 0;
-
-    requestAnimationFrame(function () {
-        currentFrame = Math.round((totalCount - 1) * per);
-        imgElem.src = videoFrame[currentFrame].src;
-    })
-}
 const intro=gsap.timeline({
     scrollTrigger:{
         trigger:'.main_section',
@@ -42,117 +15,161 @@ const intro=gsap.timeline({
         //     startColor:'blue',
         //     endColor:'red'
         // },
-        onUpdate:function(){
-            if(body.classList.contains('after-load')){
-                init()
-            }
-        }
     }
 });
-intro.to('.main_txt',0.5,{
-    opacity:0
-})
-const introduce = gsap.timeline({
+intro.to('.name',0.1,{y:160,opacity:1, autoAlpha: 1, ease:"bounce.out"})
+.to('.circle_bg',0.3,{y:-20},0.3)
+.to('.circle_bg',0.2,{opacity:0})
+.to('.arrow',0.1,{opacity:1,y:20})
+.to('.wave',0.3,{y:-400})
+.to('.wave_bg',0.3,{x:70})
+.to('.name',0.3,{y:0,opacity:0})
+
+// introduce
+// const introduce = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: '.fix.introduce',
+//         pin: '.fix.introduce',
+//         start: 'top top',
+//         end: '+=1000',
+//         scrub:1
+//     }
+// });
+// introduce.to('body',0.3,{backgroundColor:'#BBDEFB'})
+// .to('.introduce .bg_main',0.5,{ opacity: 1,scale:2,ease:"circ.out"})
+// .to('.introduce .txt-1',0.3,{opacity:1})
+// .to('.introduce .txt-1',0.3,{top:'3%'},'-=0.2')
+// .to('.main .person',0.5,{opacity:1,scale:0.9},'-=0.2')
+// .to('.introduce .txt-1,.main .person',0.3,{opacity:0})
+// .to('.introduce .bg_main',0.3,{opacity:0},'-=0.1')
+// .to('.introduce .bg_work',0.3,{opacity:1},'-=0.2')
+// .to('.introduce .txt-2',0.3,{opacity:1})
+// .to('.introduce .txt-2',0.3,{top:'2%'},'-=0.2')
+// .to('body',0.3,{backgroundColor:'#EDE7F6'})
+// .to('.work .person',0.5,{opacity:1,scale:1.1},'-=0.2')
+// .to('.work .person',0.4,{opacity:0})
+// .to('.introduce .txt-3',0.3,{opacity:1})
+// .to('.introduce .txt-3',0.3,{top:'15%'},'-=0.2')
+// .to('.qna .person',0.4,{opacity:1},'-=0.1')
+// .to('.introduce .word01',0.3,{opacity:1})
+// .to('.introduce .word01',0.3,{y:-10},'-=0.2')
+// .to('.introduce .word02',0.3,{opacity:1})
+// .to('.introduce .word02',0.3,{y:-10},'-=0.2')
+// .to('.introduce .word03',0.3,{opacity:1})
+// .to('.introduce .word03',0.3,{y:-10},'-=0.2')
+// .to('.introduce .txt-2,.introduce .txt-3,.word01,.word02,.word03',0.3,{opacity:0,y:100})
+// .to('.introduce .bg_work,.qna .person',0.3,{opacity:0})
+// // .to('.introduce .house',0.3,{opacity:1,scale:1.5})
+// .addLabel("start")
+// .to('.introduce .txt-4',0.3,{opacity:1,y:20})
+// .to('.introduce .main-circle',0.3,{opacity:1})
+// .to('.introduce .circle-container,.introduce .line1',0.7,{rotate:'360deg',repeat:4})
+// .to('.introduce .line2',0.7,{rotate:'10deg'},'-=0.6')
+// .to('.introduce .circle1,.summer',0.2,{opacity:1},'-=1.5')
+// .to('.introduce .circle2,.fall',0.2,{opacity:1},'-=1')
+// .to('.winter',0.2,{opacity:1},'-=0.2')
+// .addLabel("end")
+// .to('.introduce .line1,.center-circle,.introduce .line2',0.6,{opacity:1},'-=3.5')
+// .to('.work .person',0.1,{opacity:1},'-=3')
+// .to('.introduce .txt-5',0.3,{opacity:1, top:'15%'},'-=0.2')
+// .to('.introduce .slide,.main-circle,.small-circle,.txt-4,.txt-5',0.3,{opacity:0})
+// .to('.work .person',0.3,{opacity:0})
+// .to('.introduce .txt-10',0.3,{opacity:1,top:'10%'})
+// .to('.office',0.3,{opacity:1})
+// .to('body',0.3,{backgroundColor:'#222'})
+// advantage
+const advantage = gsap.timeline({
     scrollTrigger: {
-        trigger: '.fix.introduce',
-        pin: '.fix.introduce',
+        trigger: '.fix.advantage',
+        pin: '.fix.advantage',
         start: 'top top',
-        end: '+=18000',
+        end: '+=1500',
         scrub:1
     }
 });
-introduce.to('body',0.3,{backgroundColor:'#BBDEFB'})
-.to('.introduce .bg_main',0.5,{ opacity: 1,scale:2,ease:"circ.out"})
-.to('.introduce .txt-1',0.3,{opacity:1})
-.to('.introduce .txt-1',0.3,{top:'3%'},'-=0.2')
-.to('.main .person',0.5,{opacity:1,scale:0.9},'-=0.2')
-.to('.introduce .txt-1,.main .person',0.3,{opacity:0})
-.to('.introduce .bg_main',0.3,{opacity:0},'-=0.1')
-.to('.introduce .bg_work',0.3,{opacity:1},'-=0.2')
-.to('.introduce .txt-2',0.3,{opacity:1})
-.to('.introduce .txt-2',0.3,{top:'2%'},'-=0.2')
-.to('body',0.3,{backgroundColor:'#EDE7F6'})
-.to('.work .person',0.5,{opacity:1,scale:1.1},'-=0.2')
-.to('.work .person',0.4,{opacity:0})
-.to('.introduce .txt-3',0.3,{opacity:1})
-.to('.introduce .txt-3',0.3,{top:'15%'},'-=0.2')
-.to('.qna .person',0.4,{opacity:1},'-=0.1')
-.to('.introduce .word01',0.3,{opacity:1})
-.to('.introduce .word01',0.3,{y:-10},'-=0.2')
-.to('.introduce .word02',0.3,{opacity:1})
-.to('.introduce .word02',0.3,{y:-10},'-=0.2')
-.to('.introduce .word03',0.3,{opacity:1})
-.to('.introduce .word03',0.3,{y:-10},'-=0.2')
-.to('.introduce .txt-2,.introduce .txt-3,.word01,.word02,.word03',0.3,{opacity:0,y:100})
-.to('.introduce .bg_work,.qna .person',0.3,{opacity:0})
-// .to('.introduce .house',0.3,{opacity:1,scale:1.5})
-.addLabel("start")
-.to('.introduce .txt-4',0.3,{opacity:1,y:20})
-.to('.introduce .main-circle',0.3,{opacity:1})
-.to('.introduce .circle-container,.introduce .line1',0.7,{rotate:'360deg',repeat:4})
-.to('.introduce .line2',0.7,{rotate:'10deg'},'-=0.6')
-.to('.introduce .circle1,.summer',0.2,{opacity:1},'-=1.5')
-.to('.introduce .circle2,.fall',0.2,{opacity:1},'-=1')
-.to('.winter',0.2,{opacity:1},'-=0.2')
-.addLabel("end")
-.to('.introduce .line1,.center-circle,.introduce .line2',0.6,{opacity:1},'-=3.5')
-.to('.work .person',0.1,{opacity:1},'-=3')
-.to('.introduce .txt-5',0.3,{opacity:1, top:'15%'},'-=0.2')
-.to('.introduce .slide,.main-circle,.small-circle,.txt-4,.txt-5',0.3,{opacity:0})
-.to('.work .person',0.3,{opacity:0})
-.to('.introduce .txt-10',0.3,{opacity:1,top:'10%'})
-.to('.office',0.3,{opacity:1})
-.from('.advantage .fr .number',0.3,{opacity:0,y:-100})
-.from('.advantage .txt-11',0.3,{opacity:0,y:-100})
-.from('.advantage .sec .number',0.3,{opacity:0,y:-100})
-.from('.advantage .txt-12',0.3,{opacity:0,y:-100})
-.from('.advantage .th .number',0.3,{opacity:0,y:-100})
-.from('.advantage .txt-13',0.3,{opacity:0,y:-100})
-.to('.advantages,.txt-10',0.3,{opacity:0})
-.to('.txt-14',0.3,{opacity:1,top:'10%'})
-.to('.office',0.6,{scale:1.5,bottom:'200px'})
-.to('.office',0.6,{left:'-100%'})
-.to('.office',0.3,{opacity:0})
-.to('.txt-14',0.3,{opacity:0})
-.to('.introduce .txt-6',0.3,{opacity:1,top:'10%'},'-=0.2')
-.set('body',{backgroundColor:'#F3E5F5'})
-.to('.ability .person',0.3,{opacity:1,scale:0.8})
-.to('.h-box .html',0.3,{height:'93%'},'-=0.2')
-.to('.h-box span',0.3,{y:50,opacity:1},'-=0.2')
-.to('.c-box .css',0.3,{height:'88%'},'-=0.2')
-.to('.c-box span',0.3,{y:50,opacity:1},'-=0.2')
-.to('.j-box .js',0.3,{height:'84%'},'-=0.2')
-.to('.j-box span',0.3,{y:50,opacity:1},'-=0.2')
-.to('.v-box .vue',0.3,{height:'86%'},'-=0.2')
-.to('.v-box span',0.3,{y:50,opacity:1},'-=0.2')
-.to('.level-up',0.1,{y:-50,opacity:1},'-=0.2')
-.to('.level-up',0.1,{opacity:0})
-.set('.skill-box>div span',{opacity:0})
-.to('.skill-box>div .bar-container',0.3,{height:'200%'})
-.to('.ability .person',0.3,{opacity:0},'-=0.1')
-.to('.super .person',0.3,{opacity:1,scale:1.2},'-=0.2')
-.to('.introduce .txt-6',0.3,{opacity:0})
-.to('.introduce .txt-7',0.3,{opacity:1,top:'4%'})
-.to('.introduce .txt-8',0.3,{opacity:1,top:'15%'})
-.to('.phone-box',0.3,{opacity:1,y:-800,ease:Expo.easeInOut},'-=0.1')
-.to('.phone-box',0.3,{scale:1.3})
-.to('.phone-box',0.3,{y:900,ease:Expo.easeInOut})
+advantage
+.to('.advantage .main_title',0.2,{opacity:1,x:"0px"})
+.to('.txt1',0.1,{y:200, autoAlpha: 1})
+// .to('.layer1',0.1,{scale:1})
+// .to('.layer2',0.1,{scale:1})
 .to('body',0.3,{backgroundColor:'#222'})
-// projects
-gsap.from(".card_lib", {
-    scrollTrigger: ".card_lib",
-    x:200,
-    opacity: 0,
-    scrub: 3,
-    duration:1
-  });
-gsap.from(".card_memo", {
-    scrollTrigger: ".card_memo",
-    x:200,
-    opacity: 0,
-    scrub: 3,
-    duration:1
-  },0.2);
+.to('.advantages',0.3,{left:"0%"})
+.to('.text.txt-2',0.3,{opacity:1})
+advantage.to('.ad1',0.4,{scale:1.1})
+advantage.to('.text.txt-2',0.3,{opacity:0})
+advantage.to('.advantages',0.4,{left:"-100%"})
+advantage.to('.text.txt-3',0.3,{opacity:1})
+advantage.to('.ad2',0.4,{scale:1.1})
+advantage.to('.text.txt-3',0.3,{opacity:0})
+advantage.to('.advantages',0.4,{left:"-200%"})
+advantage.to('.ad3',0.4,{scale:1.1})
+advantage.to('.text.txt-4',0.3,{opacity:1})
+advantage.to('.text.txt-4',0.3,{opacity:0})
 
- setImg()
+// 프로젝트
+const project = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.fix.myproject',
+        pin: '.fix.myproject',
+        start: 'top top',
+        end: '+=500',
+        scrub:1
+    }
+});
+project.to('.myproject .main_title',0.2,{opacity:1,x:"0px"})
+project.to('.myproject .layer_bg',0.3,{opacity:0.2})
+project.to('.myproject .bulb1',0.1,{y:-140})
+project.to('.myproject .bulb2',0.1,{y:-100})
+project.to('.myproject .bulb3',0.1,{y:-70})
+project.to('.myproject .bulb1 .on',0.1,{opacity:1})
+project.to('.myproject .bulb2 .on',0.1,{opacity:1})
+project.to('.myproject .bulb3 .on',0.1,{opacity:1})
+project.to('.card.library',0.3,{opacity:1,x:"0%",y:"0%",visibility:"visible",rotate:"0deg"})
+project.to('.card.memo',0.3,{opacity:1,x:"0%",y:"0%",visibility:"visible",rotate:"0deg"})
+
+
+let prevIndex;
+for(let i=0; i<cards.length; i++){
+    console.log(cards[i],'카드좀')
+    cards[i].addEventListener("mouseenter",(e)=>{
+        e.preventDefault()
+        console.log("click")
+        myProject.classList.add(`active${i}`)
+        if(prevIndex !== i){
+            myProject.classList.remove(`active${prevIndex}`)
+        }
+        prevIndex=i
+       })
+       cards[i].addEventListener("mouseleave",e=>{
+           console.log("리브")
+            myProject.classList.remove(`active${prevIndex}`)
+       })
+}
+// cards.forEach((card,i)=>{
+//    card.addEventListener("mouseenter",(e)=>{
+//     e.preventDefault()
+//     console.log("click")
+//     myProject.classList.add(`active${i}`)
+//     if(prevIndex !== i){
+//         myProject.classList.remove(`active${prevIndex}`)
+//     }
+//     prevIndex=i
+//    })
+//    card.addEventListener("mouseleave",e=>{
+//        console.log("리브")
+//         myProject.classList.remove(`active${prevIndex}`)
+//    })
+// })
+const scrollValue = () =>{
+    const scrollHeight=document.body.scrollHeight;
+    const realHeight=scrollHeight - window.innerHeight;
+    let realRatio = window.pageYOffset/realHeight*100;
+    if(realRatio<0) realRatio=1;
+    if(realRatio>100) realRatio=100;
+
+    bar.style.width=`${realRatio}%`
+}
+window.addEventListener("scroll",scrollValue)
+
+
 })()
